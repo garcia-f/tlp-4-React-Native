d8042cccf5890310404a76577b7be27e0474cc60
Fabian <garciafabianemmanuel@gmail.com>
2024-04-15 12:45:52 -0300

    tlp-4

---
+++ src/MultiplecustomHook.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { useFetch } from "./hook/useFetch"
+
+
+
+export const MultiplecustomHook = () => {
+  
+    const { data, hasHerror, isLoading } = useFetch('https://rickandmortyapi.com/api/character');
+
+  
+
+    return (
+        <>
+            <h1>Rick and Morty</h1>
+            <hr />
+
+          {/*   { isLoading && <p>Cargando....</p> } */}
+
+            {/* {console.log(data)} */}
+            {/* <h3>{ data.results.map(( item ) => <ul key={ item.id }>
+                
+                <li>
+                    { item.name }
+                </li>                
+                <img src={item.image} alt="" />
+                <button>Eliminar</button>
+            </ul> ) }</h3> */}
+
+
+
+
+
+      
+        </>
+
+    )
+
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { MultiplecustomHook } from './MultiplecustomHook.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <MultiplecustomHook />
+  </>,
+)
